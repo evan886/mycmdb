744afbaddc18d40a167cda42e70a410a00542c01
evan <evan886@gmail.com>
2017-10-11 16:55:50 +0800

    2017年 10月 11日 星期三 16:55:50 CST

---
+++ domainNameManager/forms.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+from django import forms
+from domainNameManager.models import domainName
+
+class domainForm(forms.ModelForm):
+    class Meta:
+        model = domainName
+        #fields = ('name', 'nameDistributor','exptresDate','user','email','organization','availabity',)
+        #fields = ['group', 'server_type', 'ip', 'intraip', 'other_ip', 'port', 'is_monitoring', 'is_backup', 'config','app_name', 'role_name', 'editor']
+        fields = ['name', 'nameDistributor', 'exptresDate', 'user', 'email', 'organization'] #有一次是这个有个 , 出错
+        #fields = ['name', 'nameDistributor', 'exptresDate', 'user', 'email', 'organization', 'availabity']
+        #fields = ['name', 'nameDistributor', 'exptresDate', 'user', 'email', 'organization']
+        #fields = ('name',)  #bak  原来 是元组 改为列表 就好了 20171010
